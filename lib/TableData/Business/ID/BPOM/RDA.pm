14e74383f6916131ab00ed48098178ca08946221
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2024-05-16 14:11:21 +0700

    [ux] Add alias: TableData::Business::ID::BPOM::RDA

---
+++ lib/TableData/Business/ID/BPOM/RDA.pm
@@ -0,0 +1,27 @@
+package TableData::Business::ID::BPOM::RDA;
+
+use strict;
+
+require TableData::Business::ID::BPOM::NutritionLabelRef;
+
+use Role::Tiny::With;
+with 'TableDataRole::Business::ID::BPOM::NutritionLabelRef';
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+# STATS
+
+1;
+# ABSTRACT: Nutrients (alias for TableData::Business::ID::BPOM::NutritionLabelRef)
+
+=for Pod::Coverage ^()$
+
+
+=head1 SEE ALSO
+
+L<TableData::Business::ID::BPOM::NutritionLabelRef>
+
+=cut
