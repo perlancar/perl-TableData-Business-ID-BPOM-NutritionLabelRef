e13b1f1ce6e2dfb2c49df31c6555ef3bb6103680
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2024-05-16 14:25:40 +0700

    [ux] Add another shorter alias: TableData::Business::ID::BPOM::ALG

---
+++ lib/TableData/Business/ID/BPOM/ALG.pm
@@ -0,0 +1,32 @@
+package TableData::Business::ID::BPOM::ALG;
+
+use strict;
+
+require TableData::Business::ID::BPOM::NutritionLabelRef;
+
+use Role::Tiny::With;
+with 'TableDataRole::Business::ID::BPOM::NutritionLabelRef';
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+# STATS
+
+1;
+# ABSTRACT: Nutrients (shorter alias for TableData::Business::ID::BPOM::NutritionLabelRef)
+
+=for Pod::Coverage ^()$
+
+
+=head1 DESCRIPTION
+
+Keywords: acuan label gizi
+
+
+=head1 SEE ALSO
+
+L<TableData::Business::ID::BPOM::NutritionLabelRef>
+
+=cut
